3648181d058b76a30125b42d120277e90213a63c
vladimir <vladimir.shadymov@gmail.com>
2016-11-01 22:17:44 +0300

    4 is done

--- sem9/4c.py
+++ sem9/4c.py
@@ -54,14 +54,27 @@ def print_directory(cwd,k):
         print(' ',end='')
     print(cwd[cwd.rfind('/')+1:])
     ways = os.listdir(cwd)
-    for elem in ways:
-        if os.path.isfile(cwd+'/'+elem):
-            for i in range(k+2):
-                print(' ',end='')
-            print(elem)
+    if (args.folders_only==False):
+        for elem in ways:
+            if os.path.isfile(cwd+'/'+elem):
+                if args.include==None and args.exclude==None:
+                    for i in range(k+3):
+                        print(' ',end='')
+                    print(elem)
+                else:
+                     if args.exclude==None and args.include!=None:
+                         if elem.find(args.include)!=-1:
+                             for i in range(k+3):
+                                 print(' ',end='')
+                             print(elem)
+                     if args.exclude!=None and args.include==None:
+                         if elem.find(args.exclude)==-1:
+                             for i in range(k+3):
+                                 print(' ',end='')
+                             print(elem)
     for elem in ways:
         if os.path.isdir(cwd+'/'+elem):
-            print_directory(cwd+'/'+elem,k+2)
+            print_directory(cwd+'/'+elem,k+3)
         os.chdir(cwd)
 print(need_dir)
-print_directory(need_dir,1)
+print_directory(need_dir,2)
