b3ad4337dd2184bd1122d8a50ab601e7a6b2c2f7
vladimir <vladimir.shadymov@gmail.com>
2016-11-01 11:57:17 +0300

    cd

--- sem9/4c.py
+++ sem9/4c.py
@@ -1,10 +1,40 @@
 import sys
 import argparse
+import os
 
 parser = argparse.ArgumentParser(
     description='Дерево файловой системы'
 )
 
 parser .add_argument(
-
+    '--folders-only',
+    action='store_true'
 )
+
+parser .add_argument(
+    '-i',
+    '--include',
+    metavar='VALUE',
+    type=str,
+    action='store'
+)
+
+parser .add_argument(
+    '-e',
+    '--exclude',
+    metavar='VALUE',
+    type=str,
+    action='store'
+)
+
+parser .add_argument(
+    '-a',
+    '--all',
+    action='store_true'
+)
+
+parser .add_argument(
+    '--full-name',
+    action='store_true'
+)
+
