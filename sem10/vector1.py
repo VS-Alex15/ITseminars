36de48005adaa4772b65ebc3e036663687c8e75e
vladimir <vladimir.shadymov@gmail.com>
2016-11-08 18:08:36 +0300

    3rd is done

---
+++ sem10/vector1.py
@@ -0,0 +1,6 @@
+class Vector:
+    def __init__(self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
