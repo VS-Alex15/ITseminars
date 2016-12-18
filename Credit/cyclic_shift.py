e12d2ff3deb6c4856a51fc89a4f7b32dab85a272
vladimir <vladimir.shadymov@gmail.com>
2016-12-18 22:19:19 +0300

    cyclic shift #3

---
+++ Credit/cyclic_shift.py
@@ -0,0 +1,21 @@
+'''Циклический сдвиг чисел
+    в массиве влево
+'''
+list = [int(x) for x in input().split()]
+
+def left_shift(a):
+    '''Процедура циклического сдвига
+       элементов в непустом массиве влево
+    '''
+    assert a!=[]
+
+    n = len(a)
+
+    tmp = a[0]
+    for i in range(n-1):
+        a[i] = a[i+1]
+    a[n-1] = tmp
+
+left_shift(list)
+
+print(*list)
